841eceee785114a4fafea4053e97e8a4253afed9
adrienn timko <adrienn.timko@gmail.com>
2019-11-21 10:03:26 +0100

    added functionality

--- script.js
+++ script.js
@@ -1,64 +1,68 @@
-let gameBoard = document.querySelector('.container');
-let gameFields = document.querySelectorAll('.column');
-console.log(gameFields); //array: 0-8
+let gameFields = document.querySelectorAll('.column');        //console.log(gameFields); //array: 0-8
 let turn = 0;
 
+//------ take turns and stop it by 9:
 for (let i = 0; i < gameFields.length; i++) {
     gameFields[i].addEventListener('click', function () {
+        if (turn >= gameFields.length) return; //return: code below not happening if turn is bigger then 9
         if (turn % 2 === 0) {
             gameFields[i].innerHTML = "O";
+            checkWinner();
         } else {
             gameFields[i].innerHTML = "X";
+            checkWinner();
         }
         turn++;
     })
 };
 
+//------ refresh the game:
 let button = document.querySelector('.newGameBtn');
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
     for (let i = 0; i < gameFields.length; i++) {
-        gameFields[i].innerHTML = ""; //det är tomt i devtools :|
+        gameFields[i].innerHTML = "";
+        turn = 0; //turn to zero
     }
 });
 
+//------ check type and stop the game
+function checkWinner() {
+    let xWon = checkSame("X");
+    let oWon = checkSame("O");
+    
+    if (xWon) {
+        turn = gameFields.length; //to stop the game 
+        alert("X won");
+    } else if (oWon) {
+        turn = gameFields.length;
+        alert("O won");
+    } else if (turn === gameFields.length) {
+        alert("draw");
+    }
+}
 
-
-
-
-
-function checkBoard() {
-    let boardCells = Array.from(document.querySelectorAll('.column'));
-    console.log(boardCells); //array
+//----- check, if the winning matches are the same/true
+function checkSame(type) {
+    let gFs = document.querySelectorAll('.column'); 
     let winningMatches = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-}
 
-function checkForWinner() {
-    let winner = false;
-    //and after that?
-}
+    for (let i = 0; i < winningMatches.length; i++) {
 
+        let same = true; //winningMatches[i][j] are the same
 
-//if more X, alert Player X won, otherwise Player Y won.
+        for (let j = 0; j < winningMatches[i].length; j++) {
+            let winField = winningMatches[i][j]; 
+            let value = gFs[winField].innerHTML; // X or O 
 
-
-
-
-/*
-function game() {
-    let board;
-    let player1;
-    let player2;
-    let turn = 0;
-
-}
-
-function takeTurn() { //behövs nåt som argument?
-    if (turn % 2 === 0) {
-        player1.takeTurn();
-    } else {
-        player2.takeTurn();
+            if (value !== type) {
+                same = false;
+                break;
+            }
+        }
+        if (same === true) {
+            return true;
+        }
     }
-    turn++
-};
-*/
+    return false;
+}
